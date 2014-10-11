78a16ecabe847db568092f5e89eaf92baf6c058a
Harald Lapp <harald@octris.org>
2014-10-12 00:30:38 +0200

    bugfix: use __DIR__ to reference current directory

--- libs/app/web.class.php
+++ libs/app/web.class.php
@@ -14,8 +14,8 @@ namespace org\octris\core\app {
     use \org\octris\core\validate as validate;
     use \org\octris\core\provider as provider;
 
-    require_once('../app.class.php');
-    require_once('web/session.class.php');
+    require_once(__DIR__ . '/../app.class.php');
+    require_once(__DIR__ . '/web/session.class.php');
 
     /**
      * Core class for Web applications.
