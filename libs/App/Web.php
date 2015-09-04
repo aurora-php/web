ed21d78f8a962044eb9b458ea4b4069e005271bc
Harald Lapp <harald@octris.org>
2015-09-04 23:14:42 +0200

    renamed main app function, moved routing related code to sub-classes

--- libs/App/Web.php
+++ libs/App/Web.php
@@ -48,16 +48,9 @@ abstract class Web extends \Octris\Core\App
     /**
      * Application state.
      *
-     * @type    \Octris\Core\App\State
+     * @type    \Octris\Core\App\State|null
      */
     protected $state = null;
-
-    /**
-     * Entry page to use if no other page is loaded. To be overwritten by applications' main class.
-     *
-     * @type    string
-     */
-    protected $entry_page = '';
 
     /**
      * Constructor.
@@ -104,70 +97,38 @@ abstract class Web extends \Octris\Core\App
      */
     public function getState()
     {
+        if (is_null($this->state)) {
+            // state needs to be created first
+            $request = provider::access('request');
+
+            if ($request->isExist('state') && $request->isValid('state', validate::T_BASE64)) {
+                $this->state = state::thaw($request->getValue('state', validate::T_BASE64));
+            }
+
+            if (!is_object($this->state)) {
+                $this->state = new state();
+            }
+        }
+
         return $this->state;
     }
 
     /**
-     * Try to determine the last visited page supplied by the application state. If
-     * last visited page can't be determined (eg.: when entering the application),
-     * a new instance of the applications' entry page is created.
-     *
-     * @return  \Octris\Core\App\Page           Returns instance of determined last visited page or instance of entry page.
-     */
-    protected function getLastPage()
-    {
-        $class = (isset($this->state['__last_page'])
-                  ? $this->state['__last_page']
-                  : $this->entry_page);
-
-        $page = new $class($this);
-
-        return $page;
-    }
-
-    /**
-     * Make a page the last visited page. This method is called internally by the 'process' method
-     * before aquiring an other application page.
-     *
-     * @param   \Octris\Core\App\Page       $page           Page object to set as last visited page.
-     */
-    protected function setLastPage(\Octris\Core\App\Page $page)
-    {
-        $class = get_class($page);
-
-        $this->state['__last_page'] = $class;
-    }
-
-    /**
-     * Initialization of web application.
+     * Misc application initialization.
      */
     protected function initialize()
     {
-        $request = provider::access('request');
-
-        if ($request->isExist('state') && $request->isValid('state', validate::T_BASE64)) {
-            $this->state = state::thaw($request->getValue('state', validate::T_BASE64));
-        }
-
-        if (!is_object($this->state)) {
-            $this->state = new state();
-        }
     }
 
     /**
-     * Main application processor. This is the only method that needs to be called to
-     * invoke an application. Internally this method determines the last visited page
-     * and handles everything required to determine the next page to display.
-     *
-     * The following example shows how to invoke an application, assuming that 'test'
-     * implements an application based on \Octris\Core\App.
+     * Run the application. Example:
      *
      * <code>
-     * $app = test::getInstance();
-     * $app->process();
+     * $app = new App\Test();
+     * $app->run();
      * </code>
      */
-    public function process()
+    public function run()
     {
         $response = $this->getResponse();
 
@@ -178,9 +139,9 @@ abstract class Web extends \Octris\Core\App
 
         $this->initialize();
 
-        $content = $this->router->route();
+        $content = $this->router->route($this);
 
-        $response->setContent($next_page->render());
+        $response->setContent($content);
 
         $response->send();
     }
