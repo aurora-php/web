5b439ce55009461b1947b4a01e2e4bd136b6c2ad
Harald Lapp <harald@octris.org>
2015-09-05 10:34:46 +0200

    initial commit core service delegator class

---
+++ libs/App/Web/Service.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the 'octris/core' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Octris\Core\App\Web;
+
+/**
+ * Core service delegator.
+ *
+ * @copyright   copyright (c) 2015 by Harald Lapp
+ * @author      Harald Lapp <harald@octris.org>
+ */
+class Service extends \Octris\Core\App\Web
+{
+}
