3ff253633292a50ef765edf9eab194e9985d8998
Harald Lapp <harald@octris.org>
2015-09-08 22:06:10 +0200

    initial commit interface for writing router callback handlers

---
+++ libs/App/Web/Router/ICallbackHandler.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the 'octris/core' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Octris\Core\App\Web\Router;
+
+/**
+ * Interface for implementing routing callback handlers.
+ *
+ * @copyright   copyright (c) 2015 by Harald Lapp
+ * @author      Harald Lapp <harald@octris.org>
+ */
+interface ICallbackHandler
+{
+    /**
+     * Make class instance a callable.
+     *
+     * @param   \Octris\Core\App\Web                $app            Instance of application.
+     * @return  \Octris\Core\App\Web\Page|null                      Allowed return values.
+     */
+    public function __invoke(\Octris\Core\App\Web $app);
+}
