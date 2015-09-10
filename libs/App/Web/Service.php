d0f2ba9780ca7d757cad5ee8f63afdf09f2e0d92
Harald Lapp <h.lapp@clipdealer.de>
2015-09-10 10:44:42 +0200

    service base class implementation

--- libs/App/Web/Service.php
+++ libs/App/Web/Service.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the 'octris/core' package.
+ * This file is part of the 'octris/readline' package.
  *
  * (c) Harald Lapp <harald@octris.org>
  *
@@ -12,11 +12,73 @@
 namespace Octris\Core\App\Web;
 
 /**
- * Core service delegator.
+ * Service base class.
  *
  * @copyright   copyright (c) 2015 by Harald Lapp
  * @author      Harald Lapp <harald@octris.org>
  */
-class Service extends \Octris\Core\App\Web
+abstract class Service
 {
+    /**
+     * Application instance.
+     *
+     * @type    \Octris\Core\App\Web
+     */
+    protected $app;
+
+    /**
+     * Validation schema, to be configured by subclass.
+     *
+     * @type    array
+     */
+    protected $schema = array(
+        'type'       => \Octris\Core\Validate::T_OBJECT,
+        'properties' => array()
+    );
+
+    /**
+     * Allowed request method.
+     *
+     * @type    string
+     */
+    protected $method = \Octris\Core\App\Web\Request::METHOD_GET;
+
+    /**
+     * Constructor.
+     *
+     * @param   \Octris\Core\App\Web        $app        Application instance.
+     */
+    public function __construct(\Octris\Core\App\Web $app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * Validate.
+     *
+     * @return  array                                   Returns am array (is_valid, errors).
+     */
+    public function validate()
+    {
+        $errors = array();
+
+        // check if request method is valid for service
+        if (!($is_valid = ($this->method == $this->app->getRequest()->getRequestMethod()))) {
+            $errors[] = 'Invalid request method';
+        }
+
+        if ($is_valid) {
+            // check arguments
+            $provider = \Octris\Core\Provider::access($this->method);
+
+            list($is_valid, , $errors, $validator) = $provider->doValidate($this->schema);
+        }
+
+        return array($is_valid, $errors);
+    }
+
+    /**
+     * Run service.
+     */
+    abstract public function run();
 }
